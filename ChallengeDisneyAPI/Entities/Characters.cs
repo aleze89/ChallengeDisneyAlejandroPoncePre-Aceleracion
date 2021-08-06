c2d189dfaf7e25801a6711d7c87978b9af376cdd
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-08-06 00:00:44 -0300

    pasando a limpio

---
+++ ChallengeDisneyAPI/Entities/Characters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeDisneyAPI.Entities
+{
+    public class Characters
+    {
+        public int Id { get;set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Weight { get; set; }
+        public string History { get; set; }
+        //TODO peliculaseries
+
+    }
+}
