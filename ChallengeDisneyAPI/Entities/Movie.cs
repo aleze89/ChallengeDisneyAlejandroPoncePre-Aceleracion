f40d771051881c2897d0c57319285f4ecf906efe
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-08-06 23:27:24 -0300

    creando entidades

---
+++ ChallengeDisneyAPI/Entities/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeDisneyAPI.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int Rating { get; set; }
+       
+        //todo personajes
+
+        //todo generos
+
+    }
+}
