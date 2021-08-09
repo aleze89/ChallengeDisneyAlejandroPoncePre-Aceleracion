30de4dcb2c56e04890ca6756c53ec3cc70ffde7f
aleze89 <68794504+aleze89@users.noreply.github.com>
2021-08-09 01:23:01 -0300

    preparando el crud//ruta personajes funciona

---
+++ ChallengeDisneyAPI/Controllers/CharactersController.cs
@@ -0,0 +1,47 @@
+﻿using ChallengeDisneyAPI.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeDisneyAPI.Controllers
+{
+    [ApiController]
+    [Route("/[controller]")]
+    public class CharactersController : ControllerBase
+    {
+        private readonly ChallengeContext _context;
+        public CharactersController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+
+
+        [HttpPut]
+        public void Put()
+        {
+
+        }
+
+
+        [HttpDelete]
+        public void Delete()
+        {
+
+        }
+    }
+}
